ea30066963d8bb1089378fa626e846e88f8b8e5d
ys <13980152803@163.com>
2023-05-11 22:00:02 +0800

    Initial commit

---
+++ calc/calc.go
@@ -0,0 +1,9 @@
+package calc
+
+func Add(a, b int) int {
+	return add(a, b)
+}
+
+func add(a, b int) int {
+	return a + b
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"Test/calc"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("this is test not curent package = %T", calc.Add(1, 2))
+}
