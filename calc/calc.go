98cff8eb59da2394bb330099c31c702345eb1a3c
ys <13980152803@163.com>
2023-05-17 22:29:53 +0800

    昆明医保控件

--- calc/calc.go
+++
@@ -1,9 +0,0 @@
-package calc
-
-func Add(a, b int) int {
-	return add(a, b)
-}
-
-func add(a, b int) int {
-	return a + b
-}
--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-	"Test/calc"
 	"fmt"
+	"syscall"
+	"unsafe"
 )
 
 func main() {
-	fmt.Printf("this is test not curent package = %T", calc.Add(1, 2))
+	// 加载 CHSInterfaceYn.dll
+	dll := syscall.NewLazyDLL("D:\\医保\\pack\\CHSInterfaceYn.dll")
+
+	// 获取 Init 函数地址
+	initFunc := dll.NewProc("Init")
+
+	// 准备参数
+	fixmedins_code := "fixmedins_code"
+	infosyscode := "infosyscode"
+	infosyssign := "infosyssign"
+	url := "url"
+	var pErrMsg [256]byte
+
+	// 调用 Init 函数
+	r, _, err := initFunc.Call(
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(fixmedins_code))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(infosyscode))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(infosyssign))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(url))),
+		uintptr(unsafe.Pointer(&pErrMsg[0])),
+	)
+
+	// 检查调用结果
+	if r == 0 {
+		fmt.Println("Init failed:", err)
+		return
+	}
+
+	fmt.Println("Init success")
 }
